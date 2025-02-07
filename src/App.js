bef086f6fe27e2aaff5a9c6cd738dbb62d22f473
Richard R <58728519+rrjbca@users.noreply.github.com>
2025-02-07 16:22:56 +0000

    Chapter 2: mock blog post

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 export default function App() {
+  const post = {
+    id: 1,
+    text: 'Hello, world!',
+    timestamp: 'a minute ago',
+    author: {
+      username: 'susan',
+    },
+  }
+
   return (
-    <h1>Microblog</h1>
+    <>
+      <h1>Microblog</h1>
+      <p>
+        <b>{post.author.username}</b> &mdash; {post.timestamp}
+        <br />
+        {post.text}
+      </p>
+    </>
   );
 }
