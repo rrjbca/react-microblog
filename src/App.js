bb0df908567dc37df625d5103e4bf48950d60105
Richard R <58728519+rrjbca@users.noreply.github.com>
2025-02-08 08:30:26 +0000

    Chapter 3: use Container for blog posts

--- src/App.js
+++ src/App.js
@@ -24,19 +24,21 @@ export default function App() {
   return (
     <Container fluid className="App">
       <Header />
-      {posts.length === 0 ?
-        <p>There are no blog posts.</p>
-      :
-        posts.map(post => {
-          return (
-            <p key={post.id}>
-              <b>{post.author.username}</b> &mdash; {post.timestamp}
-              <br />
-              {post.text}
-            </p>
-          );
-        })
-      }
+      <Container>
+        {posts.length === 0 ?
+          <p>There are no blog posts.</p>
+          :
+          posts.map(post => {
+            return (
+              <p key={post.id}>
+                <b>{post.author.username}</b> &mdash; {post.timestamp}
+                <br />
+                {post.text}
+              </p>
+            );
+          })
+        }
+      </Container>
     </Container>
   );
 }
