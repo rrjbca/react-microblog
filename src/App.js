77d734577d9a3e03d65021978ef7bafcc7476382
Richard R <58728519+rrjbca@users.noreply.github.com>
2025-02-13 14:20:56 +0000

    Chapter 3: extract new Posts component

--- src/App.js
+++ src/App.js
@@ -2,27 +2,9 @@ import Container from 'react-bootstrap/Container';
 import Stack from 'react-bootstrap/Stack';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
+import Posts from './components/Posts';
 
 export default function App() {
-  const posts = [
-    {
-      id: 1,
-      text: 'Hello, world!',
-      timestamp: 'a minute ago',
-      author: {
-        username: 'susan',
-      },
-    },
-    {
-      id: 2,
-      text: 'Second post',
-      timestamp: 'an hour ago',
-      author: {
-        username: 'john',
-      },
-    },
-  ];
-
   return (
     <Container fluid className="App">
       <Header />
@@ -30,19 +12,7 @@ export default function App() {
         <Stack direction="horizontal">
           <Sidebar />
           <Container>
-            {posts.length === 0 ?
-              <p>There are no blog posts.</p>
-              :
-              posts.map(post => {
-                return (
-                  <p key={post.id}>
-                    <b>{post.author.username}</b> &mdash; {post.timestamp}
-                    <br />
-                    {post.text}
-                  </p>
-                );
-              })
-            }
+            <Posts />
           </Container>
         </Stack>
       </Container>
