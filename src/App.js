66f69b5329e5ffb3477cee72d85c00691d64cbce
Richard R <58728519+rrjbca@users.noreply.github.com>
2025-02-13 14:38:22 +0000

    Chapter 3: extract new Body component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import Container from 'react-bootstrap/Container';
-import Stack from 'react-bootstrap/Stack';
 import Header from './components/Header';
-import Sidebar from './components/Sidebar';
+import Body from './components/Body';
 import Posts from './components/Posts';
 
 export default function App() {
   return (
     <Container fluid className="App">
       <Header />
-      <Container>
-        <Stack direction="horizontal">
-          <Sidebar />
-          <Container>
-            <Posts />
-          </Container>
-        </Stack>
-      </Container>
+      <Body sidebar>
+        <Posts />
+      </Body>
     </Container>
   );
 }
